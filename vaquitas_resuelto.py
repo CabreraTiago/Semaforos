392ef7ec476cb8bb36e5c148b108bd7c7c4ba471
Tiago <tiagocabrera14@gmail.com>
2021-05-18 23:43:30 -0300

    Vaquitas resuelto parcialmente

---
+++ vaquitas_resuelto.py
@@ -0,0 +1,68 @@
+import os
+import random
+import time
+import threading
+
+inicioPuente = 10
+largoPuente = 20
+
+cantVacas = 5
+
+semaforoIndividual = threading.Semaphore(1)
+semaforoGrupal = threading.Semaphore(cantVacas)
+
+
+class Vaca(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.posicion = 0
+        self.velocidad = random.uniform(0.1, 0.9)
+
+    def avanzar(self):
+        semaforoGrupal.acquire()
+        try:
+            while self.posicion < inicioPuente:
+                time.sleep(1 - self.velocidad)
+                self.posicion += 1
+            try:
+                semaforoIndividual.acquire()
+                while inicioPuente <= self.posicion < inicioPuente + largoPuente:
+                    time.sleep(1 - self.velocidad)
+                    self.posicion += 1
+            finally:
+                semaforoIndividual.release()
+        finally:
+            semaforoGrupal.release()
+
+    def dibujar(self):
+        print(' ' * self.posicion + '🐮')
+
+    def run(self):
+        while True:
+            self.avanzar()
+
+
+vacas = []
+for i in range(cantVacas):
+    v = Vaca()
+    vacas.append(v)
+    v.start()
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def dibujarPuente():
+    print(' ' * inicioPuente + '=' * largoPuente)
+
+
+while True:
+    cls()
+    print('Apretá Ctrl + C varias veces para salir...')
+    print()
+    dibujarPuente()
+    for v in vacas:
+        v.dibujar()
+    dibujarPuente()
+    time.sleep(0.2)
